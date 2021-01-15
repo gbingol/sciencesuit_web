d974fbd0447b4a28dc3c6520703e5de22b3fd9b6
Gökhan Bingöl <gbingol@hotmail.com>
2021-01-15 14:24:47 +0100

    create data structures work begun

--- manual/workbook/createdatastructures.php
+++ manual/workbook/createdatastructures.php
@@ -4,7 +4,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Remove Duplicates</title>
+<title>Create Data Structures</title>
 
     <link href="../../css/common.css" rel="stylesheet" type="text/css" />
     
@@ -25,13 +25,8 @@
 
     
     
-<h1>Remove Duplicates</h1>
+<h1>Create Data Structures</h1>
 
-<p>
-    It is sometimes useful to remove multiple entries in the rows of a single or multiple columns. 
-    At the end of the operation only unique rows remain. Although the meaning of duplicate is intuitive, 
-    it is useful to solidify it with a couple examples cases.
-</p>
 
 
 <p>&nbsp;</p>
@@ -41,35 +36,12 @@
 
 
 
-<!-- ************************** Single Column ***************************** -->
+<!-- ************************** Array ***************************** -->
 
-<h2 id="Single_column">A) Single column</h2>
-
-<p>
-    This is the simplest and most intuitive case. In the following image, notice that #1 is duplicated.
-</p>
-
-<img src="images/removeduplicates_singlecolumn_beforeremoval.png" alt="" />
+<h2 id="array">Array</h2>
 
 
-<p>&nbsp;</p>
 
-
-<p>
-    Clicking on "Remove Duplicates" button will remove the duplicates in the selected column. 
-    After removal an information message will be displayed as shown:
-</p>
-
-<img src="images/removeduplicates_informationmessage.png" alt=""/>
-
-<p>&nbsp;</p>
-
-
-<p>As seen below, #1 is removed as it was duplicated.</p>
-
-<img src="images/removeduplicates_singlecolumn_aftermoval.png" alt=""/>
-
-    
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
@@ -79,54 +51,65 @@
 
 
 
+<!-- ************************** Vector ***************************** -->
 
-<!-- ****************************** Multiple Columns *********************** -->
-
-<h2 id="Multiple_columns">B) Multiple columns</h2>
-
-<p>
-    Here, the idea will be demonstrated with 2 columns; however, the same idea is applicable 
-    to any number of selected columns.
-    
-    In the figure below, two columns have been selected and we would like to remove the duplicates.
-</p>
-
-<img src="images/removeduplicates_multiplecolumns_beforeremoval.png" alt=""/>
-
-<p style="font-size: small;">Notice that we have 3 A's in the first column.</p>
-
-<p>&nbsp;</p>
+<h2 id="vector">Vector</h2>
 
 
-<p>After removal of duplicates, the remaining rows are shown below:</p>
-
-<img src="images/removeduplicates_multiplecolumns_afterremoval.png" alt=""/>
-
-<p>&nbsp;</p>
 
 
-<p>
-    Although initially we had 3 A's, only 1 A has been removed and 2 A's remained. 
-    Had we selected the first column only 2 A's would have been removed. 
-    However, by selecting two columns we pair the consecutive cells, 
-    therefore, the data in first row is considered as (A,1) and in the fifth row as (A, 10). 
-    Since the data pairs (A, 1) and (A, 10) are different, it was not considered as a duplicate. 
-    The rationale is extensible to n-tuples.</p>
 
-<p>&nbsp;</p>
-
-<p>
-    <span style="text-decoration: underline;"><em>Pitfall:</em></span> 
-    When comparisons are made, the values in the cells are considered as strings.
-</p>
 
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
+
+
+
+
+
+
+<!-- ************************** DataFrame ***************************** -->
+
+<h2 id="dataframe">DataFrame</h2>
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+
+
+
+
+<!-- ************************** Matrix ***************************** -->
+
+<h2 id="matrix">Matrix</h2>
+
+
+
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+
+
+
 
 
 <div class="RelatedLinks">
-    <a href="index.php">Essentials</a>
+    <a href="../std/classes/array.php">Array</a>
+    <a href="../std/classes/vector.php">Vector</a>
+    <a href="../std/classes/dataframe.php">DataFrame</a>
+    <a href="../std/classes/matrix.php">Matrix</a>
 </div>
 
 </body>
