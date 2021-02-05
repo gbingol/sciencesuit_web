bb711803ad0902a6565428ed3f6bad501438f9f4
Gökhan Bingöl <gbingol@hotmail.com>
2021-02-05 19:54:18 +0100

    cumtrapz updated with Array info and new signatures

--- manual/std/funcs/cumtrapz.php
+++ manual/std/funcs/cumtrapz.php
@@ -35,23 +35,25 @@
 
 <h1>cumtrapz</h1>
 
-<p>Computes the left-tailed cumulative area and returns a column Vector.</p>
+<p>
+    Computes the left-tailed cumulative area and returns a Vector/Array.
+</p>
 
 <p class="funcsignature">
     
-    cumtrapz(f=, v1=) &rarr; Vector <br />
+    cumtrapz([f=], [arg1=], [arg2=]) &rarr; Vector/Array <br />
     
     <br>
     
-    cumtrapz(v1=,v2=) &rarr; Vector <br />
-    
-    <br>
-    
-    cumtrapz{f=, a=, b=, inter=10} &rarr; Vector
+    cumtrapz{[f=], [a=], [b=], [inter=10], [x=], [y=]} &rarr; Vector
     
 </p>
 
+
+
 <p>&nbsp;</p>
+
+
 
 <h3>Arguments:</h3>
 
@@ -67,41 +69,128 @@
         <td>inter:</td> <td>Number of intervals in the limits [a,b]. Default value is 10 &larr; integer</td>
     </tr>
     <tr>
-        <td>v1, v2:</td> <td>Vectors, values on x- and y-axis, respectively. &larr; Vector</td>
+        <td>x, y, arg1, arg2:</td> <td>x- or y-axis values. &larr; Vector/Array</td>
     </tr>
 </table>
+
+<p>
+    If the return value is Vector or Array, then if at least 1 parameter is 
+    Vector, then returns Vector, otherwise returns an Array.
+</p>
+
 
 
 
 
 
 <p>&nbsp;</p>
+<p>&nbsp;</p>
 
 
 
-<p>Let's work on <em>f(x)=x</em><sup>2</sup>.</p>
+
+
+<details>
+    <summary><b>Possible Usage Cases</b></summary>
+
+    <ul class="linespaced">
+        <li>
+            cumtrapz(arg1=, arg2=) &rarr; Vector/Array
+        </li>
+
+        <li>
+            cumtrapz(f=, arg2=) &rarr; Vector/Array
+        </li>
+
+        <li>
+            cumtrapz(f=, a=, b=,  [N=10]) &rarr; Vector
+        </li>
+
+        <li>
+            cumtrapz{f=, a=, b=,  [inter=10]} &rarr; Vector
+        </li>
+
+        <li>
+            cumtrapz{f=, [x=], [y=] } &rarr; Vector/Array, either <em>x or y</em>
+        </li>
+
+        <li>
+            cumtrapz{x=, y= } &rarr; Vector/Array
+        </li>
+
+    </ul>
+</details>
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+<p>
+    Let's work on <em>f(x)=x</em><sup>2</sup>.
+</p>
 
 <p class="CodeCommand">
     &gt;&gt;y=x^2 <br />
     &gt;&gt;y <br />
-    1&nbsp;&nbsp;&nbsp; 4&nbsp;&nbsp;&nbsp; 9&nbsp;&nbsp;&nbsp; 16&nbsp;&nbsp;&nbsp; 25&nbsp;&nbsp;&nbsp; COL  <br />
+    1&emsp; 4&emsp; 9&emsp; 16&emsp; 25&emsp; COL  <br />
 
     <br />
 
     &gt;&gt;std.cumtrapz(x, y) <br />
-    0&nbsp;&nbsp;&nbsp; 2.5&nbsp;&nbsp;&nbsp; 9&nbsp;&nbsp;&nbsp; 21.5&nbsp;&nbsp;&nbsp; 42&nbsp;&nbsp;&nbsp; COL
+    0&emsp; 2.5&emsp; 9&emsp; 21.5&emsp; 42&emsp; COL
 </p>
 
 
 
 <p>&nbsp;</p>
+<p>&nbsp;</p>
 
 
-<p><em>cumtrapz{f=, a=, b=, inter=10}:</em></p>
+
 
 <p class="CodeCommand">
-    &gt;&gt;std.cumtrapz{f=<span class="auto-style1">function</span>(x) <span class="auto-style1">return</span> x^2 <span class="auto-style1">end</span>, a=1, b=5, inter=4} <br />
-    0&nbsp;&nbsp;&nbsp; 2.5&nbsp;&nbsp;&nbsp; 9&nbsp;&nbsp;&nbsp; 21.5&nbsp;&nbsp;&nbsp; 42&nbsp;&nbsp;&nbsp; COL </p>
+    &gt;&gt;arr=std.toarray{1, 2, 3, 4, 5} <br>
+    
+    <br>
+    
+    <span class="LuaComment">--return type is Array</span><br>
+    &gt;&gt;std.cumtrapz(<span class="LuaKeyword">function</span>(x) 
+    <span class="LuaKeyword">return</span> x^2 <span class="LuaKeyword">end</span>, arr) <br>
+    0&emsp; 2.5&emsp; 9&emsp; 21.5&emsp; 42   <br>
+
+    <br>
+    <br>
+    
+    &gt;&gt;v=std.tovector{1, 2, 3, 4, 5} <br>
+    
+    <br>
+    
+    <span class="LuaComment">--return type is Vector</span><br>
+    &gt;&gt;std.cumtrapz(<span class="LuaKeyword">function</span>(x) 
+    <span class="LuaKeyword">return</span> x^2 <span class="LuaKeyword">end</span>, v) <br>
+    0 &emsp; 2.5 &emsp; 9 &emsp; 21.5 &emsp; 42 &emsp; COL   <br>
+    
+</p>
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+
+
+<p class="CodeCommand">
+    &gt;&gt;std.cumtrapz{f=<span class="auto-style1">function</span>(x) 
+    <span class="auto-style1">return</span> x^2 <span class="auto-style1">end</span>, a=1, b=5, inter=4} <br />
+    0&emsp; 2.5&emsp; 9&emsp; 21.5&emsp; 42&emsp; COL 
+</p>
 
 
 <p>
