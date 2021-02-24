c41cfaa10d8b598a62675d77d7affcd132ad0524
Gökhan Bingöl <gbingol@hotmail.com>
2021-02-24 23:05:53 +0100

    added complex.php (body yet to be filled out)

---
+++ manual/std/classes/complex.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complex Class</title>
+
+    <link href="../../../css/common.css" rel="stylesheet" type="text/css" />
+    
+    <script src="/jsscripts/siteanalytics.js"></script>
+    
+    
+</head>
+
+
+<body>
+
+<?php
+    $mainmenuloc = $_SERVER["DOCUMENT_ROOT"]."/mainmenu.php"; 
+    include $mainmenuloc;
+?>
+    
+    
+
+
+
+
+<h1>Complex</h1>
+    
+
+
+</body>
+</html>
