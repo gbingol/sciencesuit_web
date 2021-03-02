7409ddc9aa93a56bfeae8137448a1c44bf624693
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-02 19:47:43 +0100

    new polyroots file

---
+++ manual/std/funcs/polyroots.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>polyroots</title>
+
+    <link href="../../../css/common.css" rel="stylesheet" type="text/css" />
+    
+    <script src="/jsscripts/siteanalytics.js"></script>
+    
+    
+</head>
+
+
+<body>
+
+<?php
+    $mainmenuloc = $_SERVER["DOCUMENT_ROOT"]."/mainmenu.php"; 
+    include $mainmenuloc;
+?>
+    
+    
+<script src="/jsscripts/createoutline.js"></script>
+<script>
+    window.addEventListener("load", MakeElement_Select_OutlineFromH3Tags);
+</script>
+
+
+
+<h1>polyroots</h1>
+    
+
+
+
+
+</body>
+</html>
