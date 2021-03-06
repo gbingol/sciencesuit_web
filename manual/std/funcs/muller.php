ad38750d21f9f715e70cda85807f02445b438ba4
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-06 17:09:52 +0100

    muller page added for muller's method

---
+++ manual/std/funcs/muller.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>muller</title>
+
+    <link href="../../../css/common.css" rel="stylesheet" type="text/css" />
+    
+    <script src="/jsscripts/siteanalytics.js"></script>
+    
+    
+</head>
+
+
+<body>
+
+<?php
+    $mainmenuloc = $_SERVER["DOCUMENT_ROOT"]."/mainmenu.php"; 
+    include $mainmenuloc;
+?>
+    
+    
+ 
+
+<h1>muller</h1>
+
+
+
+
+</body>
+</html>
