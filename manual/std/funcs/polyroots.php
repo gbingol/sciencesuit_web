9fd4533955da80aa29cc49ecee637e656ca78183
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-06 17:11:08 +0100

    more info to polyroots (not yet complete)

--- manual/std/funcs/polyroots.php
+++ manual/std/funcs/polyroots.php
@@ -3,10 +3,11 @@
 <head>
 	
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>polyroots</title>
 
     <link href="../../../css/common.css" rel="stylesheet" type="text/css" />
-    
+
     <script src="/jsscripts/siteanalytics.js"></script>
     
     
@@ -21,18 +22,64 @@
 ?>
     
     
-<script src="/jsscripts/createoutline.js"></script>
-<script>
-    window.addEventListener("load", MakeElement_Select_OutlineFromH3Tags);
-</script>
-
-
+ 
 
 <h1>polyroots</h1>
     
+<p>
+    Finds all the roots (real, complex) of a polynomial in the form of a<sub>n</sub>x<sup>n</sup> + ... + a<sub>0</sub>.
+</p>
+
+<p class="funcsignature">
+    polyroots(vec=) &rarr; real number / Array
+</p>
+
+<p>
+    where parameter <em>vec</em> must be a vector containing the coefficients of the polynom and the function returns either 
+    a real number or an Array of complex numbers.
+</p>
 
 
 
+<p>&nbsp;</p>
+
+
+
+<details>
+    <summary>Note on algorithm:</summary>
+
+    <p>
+        The applied algorithm depends on the order (<em>n</em>) of the polynomial.
+    </p>
+
+    <ol>
+        <li>n=1, directly solving the simple equation.</li>
+        <li>n=2 and n=3, application of the well-known equations to find the roots.</li>
+        <li>
+            n>3, forms <a href="https://en.wikipedia.org/wiki/Companion_matrix" target="_blank">companion matrix</a> 
+            and returns eigen values as roots.
+        </li>
+    </ol>
+
+
+</details>
+
+
+
+<p class="CodeCommand">
+    
+</p>
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+<div class="RelatedLinks">
+    <a href="bisection.php">bisection</a>
+    <a href="muller.php">muller</a>
+    <a href="newton.php">newton</a>
+</div>
 
 </body>
 </html>
