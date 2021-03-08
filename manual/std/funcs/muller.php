77fdd264373d3155a7667c9b56e24b587d82d800
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-08 21:54:40 +0100

    more info added to muller function

--- manual/std/funcs/muller.php
+++ manual/std/funcs/muller.php
@@ -44,7 +44,7 @@
 
 
 <p>
-    returns the <em>root,</em> and<em> number of iterations</em>.
+    returns the <em>root</em> and<em> the number of iterations</em>.
 </p>
 
 
@@ -52,7 +52,7 @@
 
 
 
-<h3>Arguments:</h3>
+<h3 id="arguments">Arguments:</h3>
 <table class="funcarguments">
     <tr>
         <td>f:</td>
@@ -93,7 +93,7 @@
         
         <li>
             The function must be comprised of elements all of which can handle complex numbers. 
-            See <a href="#example2">example #2</a>.
+            See <a href="#ex2_scenario1">example #2</a>.
         </li>
 
         <li>
@@ -120,7 +120,9 @@
 
 <h3 id="example1">Example #1</h3>
 <p>
-    Let's work on the polynom which has both real and complex roots: 2x<sup>3</sup> - 5x<sup>2</sup> + 2x - 5 = 0 
+    Let's work on the polynom which has both 
+    real and complex roots: 
+    2x<sup>3</sup> - 5x<sup>2</sup> + 2x - 5 = 0 
 </p>
 
 <p class="CodeCommand">
@@ -145,14 +147,92 @@
 </p>
 
 
+<p>Few things to notice:</p>
+
+<ol class="linespaced">
+    <li>
+        When x0 is real, the root can be real or complex.
+    </li>
+
+    <li>
+        Similar to other root finding methods, depending on the initial guess, the located root 
+        and the number of iterations might differ.
+    </li>
+</ol>
+
+
+
+
+
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
+
+
+
 
 
 
 <h3 id="example2">Example #2</h3>
 
+<p>
+    Let's work with a non-linear equation: x<sup>2</sup> - sin(x) = 0
+</p>
+
+
+<p>&nbsp;</p>
+
+<h4 id="ex2_scenario1">Getting unexpected errors</h4>
+<p class="CodeCommand">
+    &gt;&gt;f=<span class="LuaKeyword">function</span>(x) 
+    <span class="LuaKeyword">return</span> x^2 - math.sin(x) 
+    <span class="LuaKeyword">end</span> <br>
+
+    <br>
+
+    &gt;&gt;std.muller{f=f, x0=-2} <br>
+    <span style="color: red;">bad argument #1 to sin (number expected, got Complex)</span>
+</p>
+
+<p style="text-decoration: underline;">
+    Why did we get the error, although the function definition is completely correct?
+</p>
+
+<p>
+    Let's re-visit Notes #1: 
+    <em>The function must be comprised of elements all of which can handle complex numbers.</em>
+</p>
+
+<p>
+    The problem is that, the built-in function <em>math.sin</em> can only handle real numbers; however, 
+    the internal implementation of muller method requires that all pieces of the defined function 
+    should be able to handle complex numbers. So the fix is instead of using 
+    <em>math.sin</em> we will redefine the function using 
+    <a href="sin.php"><em>std.sin</em></a>.
+</p>
+
+
+
+<p>&nbsp;</p>
+
+
+
+<h4 id="ex2_scenario2">Make it work</h4>
+<p class="CodeCommand">
+    &gt;&gt;f=<span class="LuaKeyword">function</span>(x) 
+    <span class="LuaKeyword">return</span> x^2 - <mark>std</mark>.sin(x) 
+    <span class="LuaKeyword">end</span> <br>
+
+    <br>
+
+    &gt;&gt;std.muller{f=f, x0=-2} <br>
+    0 + 0i &emsp; 4
+</p>
+
+<p>
+    It is seen that changing from <em>math.sin</em> to <em>std.sin</em>, which 
+    can handle Complex numbers, eliminated the error and yielded a correct result.
+</p>
 
 </body>
 </html>
