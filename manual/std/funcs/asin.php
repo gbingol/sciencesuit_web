6a6f555e4cd7105fec16d943c408cbffbd05f4c6
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-09 22:28:09 +0100

    added more info to std.asin

--- manual/std/funcs/asin.php
+++ manual/std/funcs/asin.php
@@ -46,6 +46,47 @@
     <em>param</em> can be real or complex number, or an iteratable container.
 </p>
 
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+<h3 id="complex">Complex numbers</h3>
+
+<p class="CodeCommand">
+    &gt;&gt;c=std.Complex.new(3, 4) <br>
+    &gt;&gt;std.asin(c) <br>
+    0.633984 + 2.30551i
+</p>
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+
+<h3 id="containers">Containers</h3>
+
+<p>
+      The container must be an iteratable container.
+</p>
+
+<div class="CodeCommand">
+    &gt;&gt;t={1, -1, 0.5, -0.5} <br>
+    &gt;&gt;std.asin(t) <br>
+    1.5708 &emsp; -1.5708 &emsp; 0.523599 &emsp; -0.523599  <br>
+
+    <br>
+    <br>
+
+    &gt;&gt;v=std.util.tovector(t) <br>
+    &gt;&gt;std.asin(v) <br>
+    1.5708 &emsp; -1.5708 &emsp; 0.523599 &emsp; -0.523599 &emsp; COL
+</div>
+
 </body>
 
 </html>
