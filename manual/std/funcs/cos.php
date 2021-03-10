3fcea170aa4d7e473d0c411d4900d4db775e5110
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-10 21:55:11 +0100

    more info added to cos

--- manual/std/funcs/cos.php
+++ manual/std/funcs/cos.php
@@ -46,6 +46,50 @@
 </p>
 
 
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+<h3 id="complex">Complex numbers</h3>
+
+<p class="CodeCommand">
+    &gt;&gt;c=std.Complex.new(3, 4) <br>
+    &gt;&gt;std.cos(c) <br>
+    -27.0349 - 3.85115i
+</p>
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+
+<h3 id="containers">Containers</h3>
+
+<p>
+      The container must be an iteratable container.
+</p>
+
+<div class="CodeCommand">
+    &gt;&gt;t={3.1415, 0, std.Complex.new(1,2) } <br>
+    &gt;&gt;std.cos(t) <br>
+    -1 &emsp;  1 &emsp;  Complex  <br>
+
+    <br>
+    <br>
+
+    &gt;&gt;v=std.rand(3) <br>
+    &gt;&gt;v <br>
+    0.848325   &emsp; 0.588981  &emsp;  0.00752196   &emsp; COL <br>
+
+    <br>
+
+    &gt;&gt;std.cos(v) <br>
+    0.66124  &emsp;  0.831507  &emsp;  0.999972  &emsp;  COL
+</div>
 
 
 <p>&nbsp;</p>
