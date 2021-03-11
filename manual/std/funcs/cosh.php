a06be66cb55c7b4ca5aba2b0a5930e9bbc98b601
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-11 10:30:51 +0100

    info added to cosh

--- manual/std/funcs/cosh.php
+++ manual/std/funcs/cosh.php
@@ -51,14 +51,55 @@
 
 
 
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+<h3 id="complex">Complex numbers</h3>
+
+<p class="CodeCommand">
+    &gt;&gt;c=std.Complex.new(3, 4) <br>
+    &gt;&gt;std.cosh(c) <br>
+    -6.58066 - 7.58155i
+</p>
+
+
+
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 
+
+
+
+<h3 id="containers">Containers</h3>
+
+<p>
+      The container must be an iteratable container.
+</p>
+
+<div class="CodeCommand">
+      &gt;&gt;t={-1, 0, 1} <br>
+      &gt;&gt;std.cosh(t) <br>
+      1.54308 &emsp;  1  &emsp; 1.54308 
+
+      <br>
+      <br>
+
+      &gt;&gt;arr=std.util.toarray{-1, 0, 1, "3+4i"} <br>
+      &gt;&gt;std.cosh(arr) <br>
+      1.54308 &emsp;  1  &emsp; 1.54308 &emsp;  -6.58066 - 7.58155i 
+</div>
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 
 
 <div class="RelatedLinks">
       <a href="cos.php">std.cos</a>
+      <a href="sinh.php">std.sinh</a>
+      <a href="tanh.php">std.tanh</a>
 </div>
 
 
