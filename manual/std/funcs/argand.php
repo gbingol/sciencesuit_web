1137eb29fa2354c13c585dd3edafd6a9808eb701
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-23 11:32:43 +0100

    new argand.php file for std.argand cmd

---
+++ manual/std/funcs/argand.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>argand</title>
+
+    <link href="../../../css/common.css" rel="stylesheet" type="text/css" />
+    
+    
+    <script src="/jsscripts/siteanalytics.js"></script>
+
+    <script src="/jsscripts/matrixtotable.js"></script>
+  
+</head>
+    
+    
+<body>
+    
+
+
+<?php
+    $mainmenuloc = $_SERVER["DOCUMENT_ROOT"]."/mainmenu.php"; 
+    include $mainmenuloc;
+?>
+    
+
+
+<h1>argand</h1>
+    
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+<div class="RelatedLinks">
+      <a href="../classes/complex.php">Complex</a>
+      <a href="scatter.php">std.scatter</a>
+      <a href="boxplot.php">std.boxplot</a>
+</div>
+
+</body>
+
+</html>
