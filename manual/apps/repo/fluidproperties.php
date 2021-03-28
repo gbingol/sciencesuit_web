400b7c7746ad38dfea2bd93f1bef635431de38b8
Gökhan Bingöl <gbingol@hotmail.com>
2021-03-28 18:49:09 +0200

    images generated and some info added to fluid props app

--- manual/apps/repo/fluidproperties.php
+++ manual/apps/repo/fluidproperties.php
@@ -5,7 +5,7 @@
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Thermodynamic Properties of Refrigerants</title>
+    <title>Properties of Fluids</title>
     
 
     <link href="../../../css/common.css" rel="stylesheet" type="text/css" />
@@ -30,57 +30,30 @@
 ?>
 
 
-<h1>Thermodynamic Properties of Refrigerants</h1>
+<h1>Properties of Fluids</h1>
 
 <p>
-    Compute thermodynamic properties of different fluids.
+    Compute thermodynamic and heat transfer properties of different fluids. The 
+    app is comprised of 3 tabs:
 </p>
+
+<ol class="linespaced">
+    <li><a href="#saturated">Saturated Properties</a></li>
+    <li>Superheated Properties</li>
+    <li>Heat Transfer Properties</li>
+</ol>
 
 
 <p>&nbsp;</p>
 
+
+
+<h3 id="saturated">Saturated Properties</h3>
+
 <p>
-    The <em>Temperature</em> and/or <em>Pressure</em> must be selected before proceeding with computations.
+    Properties of refrigerants at saturated state are computed.
 </p>
 
-
-<img src="../images/thermodynamicpropoffluids_init.png" alt=""/>
-
-
-<p>&nbsp;</p>
-
-<p>
-    If either <em>temperature</em> or <em>pressure</em> is selected, then the <em>FLUID STATE</em> is saturated and 
-    therefore, the saturated table of the selected fluid is used to compute the properties. 
-    Since the fluid is at saturated state, it is seen that both saturated liquid and saturated vapor states are calculated.
-</p>
-
-
-<img src="../images/thermodynamicpropoffluids_saturated.png" alt=""/>
-
-
-<p>&nbsp;</p>
-
-<p>
-    If <span class="auto-style1">both</span> <em>temperature</em> and <em>pressure</em> are selected, then the 
-    <em>FLUID STATE</em> is unknown as it can be either compressed or superheated.</p>
-
-
-<img src="../images/thermodynamicpropoffluids_unknown.png" alt="" />
-<p>
-    <em>Note:</em> Although the state can also be saturated, this only happens when the saturation pressure and temperature is 
-    exactly equal to the entered values for pressure and temperature, respectively. Therefore, it is recommended 
-    to check the saturation temperature or pressure before proceeding with selection of both properties.
-</p>
-
-
-<p>&nbsp;</p>
-
-<p>
-    If the <em>FLUID STATE</em> is superheated, then the properties are computed using the superheated table of the selected fluid.
-</p>
-
-<img src="../images/thermodynamicpropoffluids_superheated.png" alt="" />
 
 
 
