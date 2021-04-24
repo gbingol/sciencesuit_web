22715fe0cbf933eae812b1e01b016379f7359c7e
Gökhan Bingöl <gbingol@hotmail.com>
2021-04-24 14:03:05 +0200

    more info added to polynomial class

--- manual/std/classes/polynomial.php
+++ manual/std/classes/polynomial.php
@@ -31,7 +31,7 @@
 <h1>Polynomial</h1>
     
 <p>
-    A class to work with polynomials, which is 
+    A class to work with 1D polynomials, which is 
     in the form of a<sub>n</sub>x<sup>n</sup> +  a<sub>n-1</sub>x<sup>n-1</sup> + ... + a<sub>0</sub>
 </p>
 
@@ -138,10 +138,7 @@
 
 <ol class="linespaced">
     <li>
-        Polynomial <b>op</b> Polynomial = Polynomial <br>
-
-        (uses <a href="https://www.boost.org/doc/libs/1_76_0/libs/math/doc/html/math_toolkit/polynomials.html"
-        target="_blank">boost library</a>)
+        Polynomial <b>op</b> Polynomial = Polynomial
     </li>
     <li>Real number <b>op</b> Polynomial = Polynomial.</li>
     <li>Polynomial <b>op</b> Real number = Polynomial</li>
@@ -187,7 +184,14 @@
     <br>
 
     &gt;&gt;p1/p2 <br>
-    0.5x + 0.25
+    0.5x + 0.25 <br>
+
+    <br>
+    <br>
+
+    &gt;&gt;p2/p1 <br>
+    <span style="color: red;">Degree of dividend is smaller than divisor.</span>
+
 </p>
 
 
@@ -221,7 +225,26 @@
 </p>
 
 <p class="CodeCommand">    
+    &gt;&gt;p1=std.Polynomial.new{1, 2, 3} <br>
+    &gt;&gt;p1 <br>
+    x^2 + 2x + 3 <br>
+
+    <br>
+
+    &gt;&gt;p1:coeffs() <br>
+    1 &emsp; 2 &emsp; 3  &emsp; COL <br>
+
+    <br>
+    <br>
     
+    &gt;&gt;p2=std.Polynomial.new{1} <br>
+    &gt;&gt;p2 <br>
+    1 <br>
+
+    <br>
+
+    &gt;&gt;p2:coeffs() <br>
+    1
 </p>
 
 
@@ -235,35 +258,6 @@
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 
-
-
-
-
-
-
-
-<h3 class="head_l3" id="clone">clone</h3>
-<p>
-    Makes an exact copy.
-</p>
-
-<p class="funcsignature">
-    clone() &rarr; Polynomial
-</p>
-
-<p class="CodeCommand">
-    
-</p>
-
-
-
-
-
-
-
-
-<p>&nbsp;</p>
-<p>&nbsp;</p>
 
 
 
@@ -282,8 +276,44 @@
 </p>
 
 <p class="CodeCommand">
-    
+    &gt;&gt;p0=std.Polynomial.new{1} <br>
+    &gt;&gt;p0 <br>
+    1 <br>
+
+    <br>
+
+    &gt;&gt;p0:degree() <br>
+    0
+
+
+    <br>
+    <br>
+    <br>
+
+    &gt;&gt;p1=std.Polynomial.new{1, 2} <br>
+    &gt;&gt;p1 <br>
+    x + 2 <br>
+
+    <br>
+
+    &gt;&gt;p1:degree() <br>
+    1 <br>
+
+    <br>
+    <br>
+    <br>
+
+
+    &gt;&gt;p2=std.Polynomial.new{1, 2, 3}  <br>
+    &gt;&gt;p2  <br>
+    x^2 + 2x + 3  <br>
+
+    <br>
+
+    &gt;&gt;p2:degree()
+    2
 </p>
+
 
 
 
@@ -305,19 +335,145 @@
 </p>
 
 <p class="funcsignature">
-    deriv(m=1) &rarr; Polynomial <br>
+    deriv(m=1) &rarr; Polynomial 
+</p>
+
+<p>
+    where parameter <em>m</em> is the order of derivative and must be &ge;1 
+</p>
+
+
+
+<p class="CodeCommand">
+    &gt;&gt;p=std.Polynomial.new{1, 2, 3, 4} <br>
+    &gt;&gt;p <br>
+    x^3 + 2x^2 + 3x + 4 <br>
 
     <br>
 
-    deriv{m=1} &rarr; Polynomial
+    &gt;&gt;p:deriv() <br>
+    3x^2 + 4x + 3 <br>
+
+    <br>
+
+    &gt;&gt;p:deriv(2) <br>
+    6x + 4 <br>
+
+    <br>
+
+    &gt;&gt;p:deriv(3) <br>
+    6 <br>
+
+    <br>
+
+    &gt;&gt;p:deriv(4) <br>
+    0 <br>
+
+    <br>
+
+    &gt;&gt;p:deriv(5) <br>
+    0
+</p>
+
+
+
+
+
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+
+
+<h3 class="head_l3" id="eval">eval</h3>
+
+<p>
+    Evaluates the polynomial.
+</p>
+
+<p class="funcsignature">
+    eval (param=) &rarr; type(param)
+</p>
+
+<p>
+    where parameter <em>param</em> can be real or complex number, 
+    Lua table (all entries must be valid real numbers) or Vector.
+</p>
+
+
+<p>&nbsp;</p>
+
+
+<details>
+    <summary>Note on algorithm:</summary>
+    <p>
+        The function uses 
+        <a href="https://en.wikipedia.org/wiki/Horner's_method" target="_blank">Horner's method</a> 
+        to evaluate the polynom.
+    </p>
+</details>
+
+
+<p>&nbsp;</p>
+
+
+
+<p class="CodeCommand">
+    &gt;&gt;p=std.Polynomial.new{1, 0, 2} <br>
+    &gt;&gt;p <br>
+    x^2 + 2 <br>
+
+    <br>
+
+    &gt;&gt;p:eval(3) <br>
+    11 <br>
+
+    <br>
+
+    &gt;&gt;p:eval{1, 2, 3} <br>
+    3 &emsp; 6 &emsp; 11 <br>
+
+    <br>
+
+    &gt;&gt;p:eval(std.util.tovector{1, 2, 3}) <br>
+    3 &emsp; 6 &emsp; 11 &emsp; COL <br>
+
+    <br>
+
+    &gt;&gt;p:eval(std.Complex.new(1,1)) <br>
+    2 + 2i
+</p>
+
+
+<p>&nbsp;</p>
+
+<p>
+    Please note that instead of p:eval(param), it is also possible to use p(param).
 </p>
 
 <p class="CodeCommand">
-    
+    &gt;&gt;p(3) <br>
+    11 <br>
+
+    <br>
+
+    &gt;&gt;p{1, 2, 3} <br>
+    3 &emsp; 6 &emsp; 11 <br>
+
+    <br>
+
+    &gt;&gt;p(std.util.tovector{1, 2, 3}) <br>
+    3 &emsp; 6 &emsp; 11 &emsp; COL <br>
+
+    <br>
+
+    &gt;&gt;p(std.Complex.new(1,1)) <br>
+    2 + 2i
 </p>
-
-
-
 
 
 
@@ -366,15 +522,177 @@
 <h3 class="head_l3" id="roots">roots</h3>
 
 <p>
-    Finds the roots of the polynomial.
+    Finds all the roots (real, complex).
 </p>
 
 <p class="funcsignature">
-    
+    roots() &rarr; real number / Array
+</p>
+
+<details>
+    <summary>Note on algorithm:</summary>
+
+    <p>
+        The applied algorithm depends on the order (<em>n</em>) of the polynomial.
+    </p>
+
+    <ol>
+        <li>n=1, directly solving the simple equation.</li>
+        <li>n=2 and n=3, application of the well-known equations to find the roots.</li>
+        <li>
+            n>3, forms <a href="https://en.wikipedia.org/wiki/Companion_matrix" target="_blank">companion matrix</a> 
+            and returns eigen values as roots.
+        </li>
+    </ol>
+
+
+</details>
+
+
+
+
+<p>&nbsp;</p>
+
+
+
+
+<h4 id="degree_1">n=1</h4>
+
+<p>
+    The return type will be a real number.
+</p>
+
+<p>
+    Working on the simple equation: 2x - 5 = 0
 </p>
 
 <p class="CodeCommand">
-   
+    &gt;&gt;p=std.Polynom.new{2, -5} <br>
+    &gt;&gt;p:roots() <br>
+    2.5
+</p>
+
+
+
+
+<p>&nbsp;</p>
+
+
+
+
+
+<h4 id="degree_2">n=2</h4>
+<p>
+    The return value will be an array containing real/complex numbers.
+</p>
+
+<p>&nbsp;</p>
+
+<p>
+    Polynom with real roots: 2x<sup>2</sup> - x - 10 = 0
+</p>
+
+<p class="CodeCommand">
+    &gt;&gt;p=std.Polynom.new{2, -1, -10} <br>
+    &gt;&gt;p:yroots() <br>
+    2.5 &emsp; -2
+</p>
+
+
+<p>&nbsp;</p>
+
+
+<p>
+    Polynom with complex roots: x<sup>2</sup> + 1 = 0
+</p>
+
+<p class="CodeCommand">
+    &gt;&gt;p=std.Polynom.new{1, 0, 1} <br>
+    &gt;&gt;p:roots() <br>
+    i &emsp;  -i   
+</p>
+
+
+
+
+
+<p>&nbsp;</p>
+
+
+
+
+
+
+<h4 id="degree_3">n=3</h4>
+<p>
+    The return value will be an array containing real/complex numbers.
+</p>
+
+<p>&nbsp;</p>
+
+<p>
+    Polynom with real and complex roots: 2x<sup>3</sup> - 5x<sup>2</sup> + 2x - 5 = 0
+</p>
+
+<p class="CodeCommand">
+    &gt;&gt;p=std.Polynom.new{2, -5, 2, -5} <br>
+    &gt;&gt;p:roots() <br>
+    2.5 &emsp; i &emsp; -i
+</p>
+
+
+<p>&nbsp;</p>
+
+
+<p>
+    Polynom with only real roots: x<sup>3</sup> - 6x<sup>2</sup> + 11x - 6 = 0
+</p>
+
+<p class="CodeCommand">
+    &gt;&gt;p=std.Polynom.new{1, -6, 11, -6} <br>
+    &gt;&gt;p:roots() <br>
+    1 &emsp; 3 &emsp; 2   
+</p>
+
+
+
+
+
+
+<p>&nbsp;</p>
+
+
+
+
+
+<h4 id="degree_4">n=4</h4>
+<p>
+    The return value will be an array containing complex numbers.
+</p>
+
+<p>&nbsp;</p>
+
+<p>
+    Polynom with only complex roots: x<sup>4</sup> + 1 = 0
+</p>
+
+<p class="CodeCommand">
+    &gt;&gt;p=std.Polynom.new{1, 0, 0, 0, 1} <br>
+    &gt;&gt;p:roots() <br>
+    -0.707107 + 0.707107i &emsp; -0.707107 - 0.707107i &emsp;  0.707107 + 0.707107i &emsp;  0.707107 - 0.707107i
+</p>
+
+
+<p>&nbsp;</p>
+
+<p>
+    Polynom with real and complex roots: 2x<sup>4</sup> + x<sup>2</sup> - 1 = 0
+</p>
+
+<p class="CodeCommand">
+    &gt;&gt;p=std.Polynom.new{2, 0, 1, 0, -1} <br>
+    &gt;&gt;p:roots() <br>
+    1.27202i &emsp;  - 1.27202i &emsp;  0.786151  &emsp; -0.786151  
 </p>
 
 
@@ -383,14 +701,23 @@
 
 
 
+
+
+
+
 <p>&nbsp;</p>
 <p>&nbsp;</p>
+
+
+
+
 
 
 
 <div class="RelatedLinks">
     <a href="array.php">Array</a>
-    <a href="../funcs/polyroots.php">polyroots</a>
+    <a href="complex.php">Complex</a>
+    <a href="../funcs/polyfit.php">polyfit</a>
 </div>
 
 
