958af1d59f13151c7ea96d4cc6fb4fedcece6b93
Gökhan Bingöl <gbingol@hotmail.com>
2021-05-30 18:54:07 +0200

    images for qqchart and std.qqnorm and std.qqplot

---
+++ manual/std/funcs/qqnorm.php
@@ -0,0 +1,58 @@
+﻿<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>qqnorm</title>
+
+    <link href="../../../css/common.css" rel="stylesheet" type="text/css" />
+    
+    
+    <script src="/jsscripts/siteanalytics.js"></script>
+  
+</head>
+    
+    
+<body>
+
+
+<?php
+   $mainmenuloc = $_SERVER["DOCUMENT_ROOT"]."/mainmenu.php"; 
+   include $mainmenuloc;
+?>
+    
+
+
+
+<h1>qqnorm</h1>
+    
+<p>Plots quantile-quantile chart.</p>
+
+
+<p>&nbsp;</p>
+
+
+
+<h3 id="syntax">Syntax</h3> 
+
+<p class="funcsignature">
+    
+</p>
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+<div class="RelatedLinks">
+   
+</div>
+
+
+</body>
+
+</html>
