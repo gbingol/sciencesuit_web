30ecd686bc79f51df9faef2fdf29b9aa4039f304
Gökhan Bingöl <gbingol@hotmail.com>
2021-06-01 11:24:27 +0200

    plotter/qqchart completed

--- manual/plotter/qqchart/qqchart.php
+++ manual/plotter/qqchart/qqchart.php
@@ -79,7 +79,7 @@
 </script>
 
     
-<h1>Histogram</h1>
+<h1>Q-Q Chart</h1>
 
 
 <p>
@@ -90,31 +90,43 @@
 
 <p>&nbsp;</p>
 
-<p id="terminology"><b>Terminology</b></p>
 
-<ul class="linespaced">
-    
-    <li>
-        <b>Theoretical Quantile:</b> A quantile value computed using normal distribution.
-    </li>
+<details>
+    <summary>Terminology</summary>
+    <ul class="linespaced">
+        
+        <li>
+            <b>Theoretical Quantile:</b> A quantile value computed using normal distribution.
+        </li>
 
 
-    <li>
-        <b>Theoretical line:</b> The line whose coordinates computed using theoretical quantiles and 
-        sample quantiles at p={0.25, 0.75}. 
-    </li> 
-</ul>
+        <li>
+            <b>Theoretical line:</b> The line whose coordinates computed using theoretical quantiles and 
+            sample quantiles at p={0.25, 0.75}. 
+        </li> 
+    </ul>
+</details>
+
+
+<p>&nbsp;</p>
 
 
 <details>
-    <summary>Plotting positions</summary>
-    <p>
-        The following formula used to locate the plotting positions.
-    </p>
+    <summary>Technical Details</summary>
+    <ul class="linespaced">
+        <li>
+            The following formula used to locate the plotting positions: <br>
+            <em>(k - a) / (n + 1 - 2a)</em>, if n<=10 &rarr; a = 0.375, otherwise a = 0.5
+        </li>
 
-    <p>
-        <em>(k - a) / (n + 1 - 2a)</em>, if n<=10 &rarr; a = 0.375, otherwise a = 0.5
-    </p>
+        <li>
+            If a pair of data is used to construct the chart, then:<br>
+            <b>1) </b> If data lengths are equal, then sorted values are plotted against each other.<br>
+            <b>2) </b> If data lengths are unequal, then linear interpolation is used to "sample" from 
+            the data with larger size.
+        </li>
+
+    </ul>
 
 </details>
 
@@ -135,7 +147,12 @@
     Q-Q plots.
 </p>
 
+
+
 <p> &nbsp;</p>
+
+
+
 
 
 
@@ -148,9 +165,40 @@
         control = [10,17,19,20,26,28,33,37,37,41,42,42,42,43,46,48,53,54,55,55,60,62,85]   
     </span>
 </p>
-    
-</p>
 
+ <p>
+    Assuming that the data has already been entered to worksheet, there are two cases: 
+ </p>  
+
+ <ol class="linespaced">
+    <li>Selection of single column,</li>
+    <li>Selection of two columns</li>
+ </ol> 
+
+
+
+<h4 id="singlecolumn" class="head_l4">Single column</h4>
+
+<ol class="linespaced">
+    <li>Select "Control" data, optionally including the label itself.</li>
+    <li>Click on Q-Q plot icon and a chart similar to the following will be displayed:</li>
+ </ol> 
+
+ <img src="images/selection_singlecolumn.png" alt="">
+
+
+
+
+ <p>&nbsp;</p>
+
+
+ <h4 id="twocolumns" class="head_l4">Two columns</h4>
+ <ol class="linespaced">
+    <li>Select "Treatment" and "Control", optionally including the labels themselves.</li>
+    <li>Click on Q-Q plot icon and a chart similar to the following will be displayed:</li>
+ </ol>
+
+ <img src="images/selection_twocolumn.png" alt="">
 
 
 <p>&nbsp;</p>
@@ -189,93 +237,27 @@
     
 <h2 class="head_l2"  id="customizechart">Customizing the Chart</h2>
 
-
-<h3 class="head_l3"  id="optionstab">Options Tab</h3>
-
 <p>
-    The options tab simply allows you to change the way histogram is constructed.
+    Right-click on any of the marker and select "Format...". A dialog with 
+    "Line Tab" and "Marker Tab" will be shown.
 </p>
 
-<img src="images/formathistogram_optionstab.png" alt="">
-
-
-<h4 class="head_l4" id="series_distributionmode">Distribution Mode</h4>
+<h3 class="head_l3"  id="optionstab">Line Tab</h3>
 
 <p>
-    Initially the chart is plotted using the "Frequency" mode; however, 
-    the mode can be changed to "Relative Frequency" or "Density".  
-    
-    If "Density" is selected, then "Cumulative" option is disabled and unchecked, therefore, 
-    the chart will not display a cumulative distribution anymore. 
+    The line tab is enabled only when a single column is selected and 
+    is disabled when two columns are selected as shown in the following 
+    figure:
 </p>
 
-<p>
-    Below, the chart is constructed using the cumulative and relative frequency options.
-</p>
-
-<img src="images/chart_singleseries_relativefreq_cumulative.png" alt="">
-
+<img src="images/format_linetab.png" alt="">
 
 <p>&nbsp;</p>
 
 <p>
-    The following chart displays the same data in the "Density" mode.
+    When a single column is selected, the "theoretical line" can be shown or hidden 
+    and its appearance can be changed.
 </p>
-
-<img src="images/chart_singleseries_density.png" alt="">
-
-
-<p>&nbsp;</p>
-
-
-<h4 class="head_l4" id="bin_mode">Bin Method</h4>
-
-<p>
-    The default bin method is for computing the number of bins is Freedman–Diaconis rule. 
-    The number of bins can be changed and a number between [1, 100] can be selected. 
-</p>
-
-<p>
-    The following chart shows the same data using 30 bins. 
-</p>
-
-<img src="images/chart_singleseries_freq_binnum_30bins.png" alt="">
-
-
-<p>&nbsp;</p>
-
-
-<p><b>Class Intervals of Unequal Widths</b></p>
-
-<p>
-    The number of bins, whether automatically computed or manually changed,
-    results in equal class widths. However, unequal class widths can be used 
-    as well using the following steps: 
-</p>
-
-<p>
-    <b>1) </b> <a href="../../workbook/createdatastructures.php">Either using the workbook</a> 
-    or using the command-line create a vector variable holding the breaks.
-</p>
-
-<p class="CodeCommand">
-    &gt;&gt;breaks=std.util.tovector{-2,-0.5, 0, 2}
-</p>
-
-<p>
-    <span style="color: red;">Note:</span> The breakpoints must be in (min, max) data.
-</p>
-
-<p>
-    <b>2)</b> In the "Binning Method", select "Lua Variable" and type the name of the Lua variable, 
-    namely <span style="background-color:lightcyan;">breaks</span>, and press enter.
-</p>
-
-<p>
-    <b>3)</b> The following chart was obtained using the above-generated breaks and in the density mode.
-</p>
-
-<img src="images/chart_singleseries_manualbreaks_density.png" alt="">
 
 
 
@@ -283,21 +265,14 @@
 <p>&nbsp;</p>
 
 
-<h3 class="head_l3"  id="fill_linetab">Line/Fill Tab</h3>
+<h3 class="head_l3"  id="fill_linetab">Marker Tab</h3>
 <p>
-    Appearance of the bins can be formatted using the following options.
+    Appearance of markers can be formatted.
 </p>
 
-<img src="images/formathistogram_line_filltab.png" alt="">
+<img src="images/format_markertab.png" alt="">
 
-<p>&nbsp;</p>
 
-<p>
-    For example, the following chart is displayed 
-    in frequency and cumulative mode using some formatting.
-</p>
-
-<img src="images/chart_singleseries_formatted_freq_cumulative.png" alt="">
 
 
 
@@ -311,10 +286,12 @@
 
 
 <div class="RelatedLinks">
-    <a href="../../std/funcs/histogram.php">std.histogram</a>
-    <a href="../argand/argand.php">Argand Diagram</a>
+    <a href="../../std/funcs/qqnorm.php">std.qqnorm</a>
+    <a href="../../std/funcs/qqplot.php">std.qqplot</a>
+    <a href="../../std/funcs/test_norm_ad.php">std.test_norm_ad</a>
+
     <a href="../boxwhisker/boxwhisker.php">Box-Whisker Chart</a>
-    <a href="../scatter/scatterchart.php">Scatter Charts</a>
+    <a href="../histogram/histogram.php">Histogram</a>
 </div>
 
   
